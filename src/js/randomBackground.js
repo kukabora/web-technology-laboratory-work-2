1f567a584231422fd849f9f93c7e78f4ab5bd77b
Alex <youdoit@mail.ru>
2021-10-08 15:57:16 +0600

    Added one custom script on javascript

---
+++ src/js/randomBackground.js
@@ -0,0 +1,12 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let back = document.querySelector(".background");
+let preparedString = 'url("/src/img/personal-cabinet-';
+let numberOfPhoto = getRandomInt(4);
+
+
+back.style.backgroundImage = preparedString + numberOfPhoto + '.jpg")';
+back.style.backgroundSize = "cover";
+back.style.backgroundPosition = "center";
