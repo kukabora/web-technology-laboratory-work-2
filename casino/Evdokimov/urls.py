1b68914facd8baeb96d5587998c5d457ccddc17b
Alex <youdoit@mail.ru>
2021-10-30 19:13:23 +0600

    Added test page and deployed server

---
+++ casino/Evdokimov/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import testPage
+
+urlpatterns = [
+    path('', testPage)
+]
--- casino/Evdokimov/views.py
+++ casino/Evdokimov/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def testPage(request):
+    return render(request, 'Evdokimov/index.html')
